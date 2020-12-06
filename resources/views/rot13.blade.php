a2eb655bdddd84138fa3e87196c04f21e4667b0b
alvina vania kirana <alvinavania@alvinas-MacBook-Pro.local>
2020-12-06 18:54:26 +0700

    ferot13

--- resources/views/rot13.blade.php
+++ resources/views/rot13.blade.php
@@ -1,4 +1,4 @@
-<html lang="en">
+ <html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -7,7 +7,9 @@
     <title>ROT13</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 
 <!-- Navbar -->
@@ -37,27 +39,28 @@
 
     <!-- Content -->
     <div class="content">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <h2>ROT 13</h2>
-
-                <form action="{{ route('rot13') }}" method="POST">
-                    @csrf
-                    <div class="input-group pt-4">
-                        <input type="text" name="message" class="form-control" placeholder="Type your message here...">
-
-                        <button class="btn btn-primary">
-                            Encrypt
-                        </button>
-                </form>
-
-            </div>
+    <img src="{{ url('img/image1.png') }}" alt="" class="gambar float-left">
+        <div class="main-content">
+        <h1>ROT 13</h1>
+            <form action="{{ route('rot13') }}" method="POST">
+                @csrf
+                <div class="form-row mt-3">
+                    <div class="col-5 plaintext">
+                    <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Type your message here..."></textarea>
+                    </div>
+                    <button class="btn btn-submit">
+                        Encrypt
+                    </button>
+            </form>
             @if (strlen(session('encrypted')) > 0)
-                <div class="card" style="height: 500px">
-                    <h2>Result</h2>
-                    Encrypted Text : {{ session('encrypted') }}
-                    Decrypted Text : {{ session('decrypted') }}
+            <div class="main-content">
+                <div class="form-row">
+                    <ul style="list-style:none" class="hasil col-11 p-3">
+                        <li>Encrypted Text : {{ session('encrypted') }}</li>
+                        <li>Decrypted Text : {{ session('decrypted') }}</li>
+                    </ul>
                 </div>
+            </div>
             @endif
         </div>
     </div>
